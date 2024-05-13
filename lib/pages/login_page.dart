c3aa8762046089f4e2d23c96881b19bceb1503f4
Harshana Prasad <harshana7096@gmail.com>
2024-05-13 23:51:50 +0530

    created food deliver app using flutter

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class LogingPage extends StatelessWidget{
+  const LogingPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body:Column(children: [
+        //logo
+
+
+        //mesage,app slogan
+
+
+        //email textfield
+
+
+        //password textfield
+
+
+        //sign in button
+
+
+        //not a member? register now
+
+        ],
+      ),
+    );
+  }
+}
