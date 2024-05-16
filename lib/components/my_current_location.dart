acdb0df27aee23b3f20019ec846f2c59950a6846
Harshana Prasad <harshana7096@gmail.com>
2024-05-16 23:01:45 +0530

    created  dark mood function for app and add address search function
    created  dark mood function for app and add address search function  add do modification and created silver app bar

---
+++ lib/components/my_current_location.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+
+class MyCurrentLocation extends StatelessWidget {
+  const MyCurrentLocation({super.key});
+
+ void openLocationSearchBox(BuildContext context){
+  showDialog(
+    context: context,
+     builder: (context)=>AlertDialog(
+          title:const Text("Your Location"),
+          content: const TextField(decoration:const InputDecoration(hintText: "Search address.."),
+          ),
+          actions: [
+            //cancal button
+
+
+            //save button
+          ],
+       ),
+     );
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Deliver now",style:TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+          GestureDetector(
+            onTap: () => openLocationSearchBox(context),
+            child: Row(
+              children: [
+                //address
+                Text("6901 Hollywood Blv",style:TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold,
+                  
+                  ),
+                ),
+                  
+                  
+                //drop down menu
+                Icon(Icons.keyboard_arrow_down_rounded),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
