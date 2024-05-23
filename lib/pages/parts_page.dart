804fc28c6a2e3f5bcc74590a112c5606e5e278de
Harshana Prasad <harshana7096@gmail.com>
2024-05-23 21:36:28 +0530

    created modification for page and add SingleChildScrollView for page for stop pixel overflow

--- lib/pages/parts_page.dart
+++ lib/pages/parts_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:food_dilivery_app/components/my_button.dart';
 import 'package:food_dilivery_app/models/parts.dart';
 
 class PartsPage extends StatefulWidget {
@@ -20,92 +19,104 @@ class _PartsPageState extends State<PartsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:SingleChildScrollView(
-      child:Column(
-      children:[
-
-        // part image
-        Image.asset(widget.part.imagePath),
-
-        Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            //part name
-          Text(
-            widget.part.name,
-            style:const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize:20,
-              ),
-            ),
-
-           //part Price
-          Text(
-            '\LKR '+widget.part.price.toString(),
-            style:TextStyle(
-             fontSize:16,
-             color:Theme.of(context).colorScheme.primary,
-             fontWeight: FontWeight.bold),
-              ),
-
-             const SizedBox(height: 10), 
-          
-          //part description
-          Text(widget.part.description,),
-          
-
-          const SizedBox(height: 10),
-
-          Divider(color:Theme.of(context).colorScheme.secondary),
-
-          const SizedBox(height: 10),
-          
-          //addons
-          Text(
-            "Add-ons",
-            style:TextStyle(color:Theme.of(context).colorScheme.inversePrimary,
-            fontSize:16,
-            fontWeight:FontWeight.bold,
-              ),
-            ),
-
-          const SizedBox(height: 10),
-
-          Container(
-            decoration:BoxDecoration(
-              border:Border.all(
-                color: Theme.of(context).colorScheme.secondary),
-                borderRadius:BorderRadius.circular(8),
+        child: Column(
+        children:[
+        
+          // part image
+          Image.asset(widget.part.imagePath),
+        
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              //part name
+            Text(
+              widget.part.name,
+              style:const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize:20,
                 ),
-            child: ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              padding: EdgeInsets.zero,
-              itemCount: widget.part.availableAddons.length,
-              itemBuilder: (context, index) {
-              //get individual addon
-              Addon addon = widget.part.availableAddons[index];
-            
-              //retuen checkbox IU
-              return CheckboxListTile(
-                title:Text(addon.name),
-                subtitle: Text(addon.price.toString()),
-                value: false, onChanged: (value){},
-                );
-              },
-             ),
-          )
-                ],
               ),
-        ),
+        
+             //part Price
+            Text(
+              '\LKR '+widget.part.price.toString(),
+              style:TextStyle(
+               fontSize:16,
+               color:Theme.of(context).colorScheme.primary,
+               fontWeight: FontWeight.bold),
+                ),
+        
+               const SizedBox(height: 10), 
+            
+            //part description
+            Text(widget.part.description,),
+            
+        
+            const SizedBox(height: 10),
+        
+            Divider(color:Theme.of(context).colorScheme.secondary),
+        
+            const SizedBox(height: 10),
+            
+            //addons
+            Text(
+              "Add-ons",
+              style:TextStyle(color:Theme.of(context).colorScheme.inversePrimary,
+              fontSize:16,
+              fontWeight:FontWeight.bold,
+                ),
+              ),
+        
+            const SizedBox(height: 10),
+        
+            Container(
+              decoration:BoxDecoration(
+                border:Border.all(
+                  color: Theme.of(context).colorScheme.secondary),
+                  borderRadius:BorderRadius.circular(8),
+                  ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                itemCount: widget.part.availableAddons.length,
+                itemBuilder: (context, index) {
+                //get individual addon
+                Addon addon = widget.part.availableAddons[index];
+              
+                //retuen checkbox IU
+                return CheckboxListTile(
+                  title:Text(addon.name),
+                  subtitle: Text('\LKR ${addon.price}',
+                  style:TextStyle(
+                    color:Theme.of(context).colorScheme.primary),
+                      ),
+                  value: false,
+                  onChanged: (value){},
+                    );
+                  },
+               ),
+            )
+                  ],
+                ),
+          ),
+        
+        
+          // button add to the cart
+          MyButton(
+            onTap:() {},
+          text:"Add to Cart",
+          ),
 
-
-        // button add to the cart
-        ],
-       ),
+          const SizedBox(height: 25),
+        
+          ],
+         ),
       ),
-    );
+      );
+    
   }
 }
