ae7c54d0f636827dd7e15249e46f3c84ff02070c
Harshana Prasad <harshana7096@gmail.com>
2024-05-29 22:41:27 +0530

    created payment page
    add credit card payment function

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:food_dilivery_app/components/my_button.dart';
 import 'package:food_dilivery_app/components/my_cart_title.dart';
 import 'package:food_dilivery_app/models/shop.dart';
+import 'package:food_dilivery_app/pages/payment_page.dart';
 import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
@@ -84,9 +85,16 @@ class CartPage extends StatelessWidget {
               ),
 
               //button to pay
-              MyButton(onTap:() {
-
-              },text:"Go to checkout"),
+              MyButton(
+                onTap:() => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PaymentPage(),
+                  ),
+                ),
+                
+              text:"Go to checkout",
+              ),
 
               const SizedBox(height: 25),
             ],
