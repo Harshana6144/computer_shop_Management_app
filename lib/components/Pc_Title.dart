a4f6d6e8c4965a4a60af6408dac9dcfb924503cf
Harshana Prasad <harshana7096@gmail.com>
2024-07-29 19:18:09 +0530

    created search page for app

---
+++ lib/components/Pc_Title.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class PcTitle extends StatelessWidget {
+  const PcTitle ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -1,25 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
-  State<SearchPage> createState() => _MyWidgetState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _MyWidgetState extends State<SearchPage> {
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF1f1545),
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1f1545),
-        elevation: 0.0,
-      ),
-      body:Padding(
-        padding:EdgeInsets.all(16),
-      )
+    return Column(
+      children: [
+
+        //Add a SizedBox to craete for get space 
+        SizedBox(height: 100),
+
+        //search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(color:Theme.of(context).colorScheme.tertiary,
+          borderRadius: BorderRadius.circular(8)),
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search',style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),),
+              Icon(Icons.search),
+            ],
+          ),
+          
+        ),
+
+        //message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text('Everyone files..some fly longer then others',
+          style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+          ),
+        ),
+
+        //hot picks
+       const Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 25.0),
+          child:  Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children:   [
+              Text(
+                'Hot Pickes 🔥',
+                style:TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize:24,
+                ),
+              ),
+                Text('See all',style: TextStyle(fontWeight:FontWeight.bold,color: Colors.blue),
+                ),
+              ],
+            ),
+        ),
+
+        const SizedBox(height: 10),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context,index){
+                return PcTitle();
+              }
+            )
+          )
+      ],
+
     );
   }
+
 }
